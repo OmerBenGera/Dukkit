cb3c0146dea85082fe0fbff7300c97c5db8bbbce
OmerBenGera <omerthepro126@gmail.com>
2021-06-05 13:19:10 +0300

    Update logs format

--- src/main/java/org/dukkit/DukkitMain.java
+++ src/main/java/org/dukkit/DukkitMain.java
@@ -6,15 +6,22 @@ import org.dukkit.patches.parser.PatchParser;
 import java.io.File;
 import java.io.IOException;
 import java.lang.instrument.Instrumentation;
+import java.util.Date;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public final class DukkitMain {
 
-    private static final Logger logger = Logger.getLogger("Dukkit");
+    private static final Logger logger = initLogger();
     private static final String PATCHES_PATH = "patches";
 
     public static void premain(String agentArgs, Instrumentation instrumentation) {
         try {
+            long startTime = System.currentTimeMillis();
+            logger.info("Starting Dukkit...");
+
             DukkitTransformer transformer = new DukkitTransformer();
 
             File patchesFolder = createPatchesFolder();
@@ -35,11 +42,11 @@ public final class DukkitMain {
                 String patchFileName = patchFile.getName();
                 try {
                     logger.info(String.format("Loading patch %s...", patchFileName));
-                    long startTime = System.currentTimeMillis();
+                    long patchStartTime = System.currentTimeMillis();
                     transformer.addPatch(PatchParser.fromFile(patchFile).parse());
-                    long endTime = System.currentTimeMillis();
+                    long patchEndTime = System.currentTimeMillis();
                     logger.info(String.format("Loading patch %s is done (Took %dms)!",
-                            patchFileName, endTime - startTime));
+                            patchFileName, patchEndTime - patchStartTime));
                 } catch (IOException ex) {
                     logger.warning(String.format("Cannot read the patch '%s'.", patchFileName));
                 } catch (InvalidPatchException ex) {
@@ -48,6 +55,10 @@ public final class DukkitMain {
             }
 
             instrumentation.addTransformer(transformer);
+
+            long endTime = System.currentTimeMillis();
+
+            logger.info(String.format("Dukkit was successfully done (Took %dms)!", endTime - startTime));
         }catch (Exception ex){
             logger.warning("An unexpected error occurred while running Dukkit:");
             ex.printStackTrace();
@@ -67,6 +78,32 @@ public final class DukkitMain {
         }
 
         return patchesFolder;
+    }
+
+    private static Logger initLogger(){
+        if(DukkitMain.logger != null){
+            return DukkitMain.logger;
+        }
+
+        Logger logger = Logger.getLogger("Dukkit");
+        logger.setUseParentHandlers(false);
+
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setFormatter(new SimpleFormatter() {
+            private static final String LOG_FORMAT = "[%1$tT %2$s]: %3$s %n";
+
+            @Override
+            public synchronized String format(LogRecord lr) {
+                return String.format(LOG_FORMAT,
+                        new Date(lr.getMillis()),
+                        lr.getLevel().getLocalizedName(),
+                        lr.getMessage()
+                );
+            }
+        });
+        logger.addHandler(handler);
+
+        return logger;
     }
 
 }
